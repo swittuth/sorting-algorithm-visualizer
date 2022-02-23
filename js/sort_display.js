06aa9d8058a1781862858aaddea3072978bc340d
swittuth <swittuthcoding@gmail.com>
2022-02-22 20:11:41 -0500

    create a sample display of the numbers in a bar chart form to be sorted

--- js/sort_display.js
+++ js/sort_display.js
@@ -4,8 +4,39 @@ const visual_context = visual_console.getContext('2d');
 visual_console.width = 800;
 visual_console.height = 400;
 
-visual_context.fillRect(1.111122232142, visual_console.height - 20, 2.25, 20);
-
 class Sort_Visualizer_Board {
-    
+    constructor() {
+        this.height = visual_console.height;
+        this.width = visual_console.width;
+        this.num_array = [];
+        this.num_numbers = 5;
+        this.default_size = 20;
+        this.default_spacing = 2;
+    }
+
+    initiate_random_array(){
+        for (let i = 0; i < this.num_numbers; i++){
+            this.num_array.push(Math.random() * this.height); 
+        }
+    }
+
+    display_numbers() {
+        // if number of blocks including spacing would get out of screen then decrease block size
+        // display number as height
+        // have to display in center
+        let offset_per_block = this.default_size + this.default_spacing;
+        let middle_index = Math.floor(this.num_array.length / 2);
+        let initial_position = (this.width / 2) - middle_index * offset_per_block;
+        for (let i = 0; i < this.num_array.length; i++){
+            visual_context.fillRect(initial_position, visual_console.height, this.default_size, -this.num_array[i]);
+            initial_position += offset_per_block;
+            console.log(initial_position);
+        }
+    }
 }
+
+const sort_visualizer = new Sort_Visualizer_Board();
+sort_visualizer.initiate_random_array();
+sort_visualizer.display_numbers();
+
+console.log(sort_visualizer.num_array)

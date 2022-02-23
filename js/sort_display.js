a1e533f1f9f4ff254f2327a75f388ead127c7d6b
swittuth <swittuthcoding@gmail.com>
2022-02-22 19:12:02 -0500

    created a layout for the visualizer board

---
+++ js/sort_display.js
@@ -0,0 +1,11 @@
+const visual_console = document.getElementById("visualizer");
+const visual_context = visual_console.getContext('2d');
+
+visual_console.width = 800;
+visual_console.height = 400;
+
+visual_context.fillRect(1.111122232142, visual_console.height - 20, 2.25, 20);
+
+class Sort_Visualizer_Board {
+    
+}
